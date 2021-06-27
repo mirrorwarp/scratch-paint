e9ee647f8c11db726b4e15a51ee233113c2f087b
Thomas Weber <weber_t@mailbox.org>
2021-06-27 13:41:43 -0500

    Let scratch-gui deal with all custom stage size parsing

--- src/lib/tw-stage-size.js
+++ src/lib/tw-stage-size.js
@@ -1,49 +1,5 @@
-// Keep in sync with TW/scratch-gui
-
-// Some limits to make sure the interface doesn't completely fall apart
-const MIN_WIDTH = 0;
-const MIN_HEIGHT = 0;
-
-const MAX_WIDTH = 4096;
-const MAX_HEIGHT = 4096;
-
-const DEFAULT_WIDTH = 480;
-const DEFAULT_HEIGHT = 360;
-
-const PARAM = 'size';
-
-const getDimensions = () => {
-    // Running in node.js
-    if (typeof URLSearchParams === 'undefined') {
-        return {
-            width: DEFAULT_WIDTH,
-            height: DEFAULT_HEIGHT
-        };
-    }
-
-    const urlParameters = new URLSearchParams(location.search);
-    if (!urlParameters.has(PARAM)) {
-        return {
-            width: DEFAULT_WIDTH,
-            height: DEFAULT_HEIGHT
-        };
-    }
-
-    const dimensionsQuery = urlParameters.get(PARAM);
-    const [_, widthText, heightText] = dimensionsQuery.match(/^(\d+)[^\d]+(\d+)$/);
-    if (!widthText || !heightText) {
-        return {
-            width: DEFAULT_WIDTH,
-            height: DEFAULT_HEIGHT
-        };
-    }
-
-    const width = Math.max(MIN_WIDTH, Math.min(MAX_WIDTH, +widthText));
-    const height = Math.max(MIN_HEIGHT, Math.min(MAX_HEIGHT, +heightText));
-    return {
-        width,
-        height
-    };
+// scratch-gui may import and modify this before loading the rest of scratch-paint
+export default {
+    width: 480,
+    height: 360
 };
-
-export default getDimensions();
