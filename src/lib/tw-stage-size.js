da65ae744f3d3a9a22378427c67f4c0ee99e15ac
Thomas Weber <weber_t@mailbox.org>
2021-06-27 13:51:49 -0500

    Fix test errors

--- src/lib/tw-stage-size.js
+++ src/lib/tw-stage-size.js
@@ -1,5 +1,7 @@
 // scratch-gui may import and modify this before loading the rest of scratch-paint
-export default {
+// we use old-style export here because it fixes syntax errors in tests
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
     width: 480,
     height: 360
 };
