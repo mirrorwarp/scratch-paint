b2bad33025762a49bb9651fb921a877a8df74169
Thomas Weber <muffin@muffin.ink>
2022-03-14 22:24:37 -0500

    Revert "Don't change format until image is imported"
    This reverts commit 8b1f591033006872086d0b59113c34a4b466ccf2.
    
    This may be related to some image-corrupting undo/redo bugs.

--- src/containers/paper-canvas.jsx
+++ src/containers/paper-canvas.jsx
@@ -144,6 +144,7 @@ class PaperCanvas extends React.Component {
 
         if (format === 'jpg' || format === 'png') {
             // import bitmap
+            this.props.changeFormat(Formats.BITMAP_SKIP_CONVERT);
             setImportingImage(true);
 
             const mask = new paper.Shape.Rectangle(getRaster().getBounds());
@@ -158,7 +159,6 @@ class PaperCanvas extends React.Component {
                 if (!this.queuedImageToLoad) return;
                 this.queuedImageToLoad = null;
 
-                this.props.changeFormat(Formats.BITMAP_SKIP_CONVERT);
                 this.clearPaperCanvas();
                 setImportingImage(false);
 
@@ -180,6 +180,7 @@ class PaperCanvas extends React.Component {
             };
             imgElement.src = image;
         } else if (format === 'svg') {
+            this.props.changeFormat(Formats.VECTOR_SKIP_CONVERT);
             this.importSvg(image, rotationCenterX, rotationCenterY);
         } else {
             this.clearPaperCanvas();
@@ -253,7 +254,6 @@ class PaperCanvas extends React.Component {
         });
     }
     initializeSvg (item, rotationCenterX, rotationCenterY, viewBox) {
-        this.props.changeFormat(Formats.VECTOR_SKIP_CONVERT);
         setImportingImage(false);
         this.clearPaperCanvas();
 
